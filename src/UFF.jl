e7e0236dd9ecf9490ce5a7726b94c9044c7299c6
Simon Andreas Bjørn <simonabj@ifi.uio.no>
2023-09-13 13:32:52 +0200

    Create uff file placeholders

--- src/UFF.jl
+++ src/UFF.jl
@@ -2,8 +2,29 @@ module UFF
 
 version = v"1.2.0"
 
+# Base types
 include("structs/Wavefront.jl")
 include("structs/Window.jl")
+include("structs/Point.jl")
+
+# Scan
+include("structs/Scan.jl")
+include("structs/LinearScan.jl")
+include("structs/SectorScan.jl")
+
+# Probes
+include("structs/Probe.jl")
+include("structs/LinearArray.jl")
+include("structs/CurvilinearArray.jl")
+
+# Compund Structs
+include("structs/Pulse.jl")
+include("structs/Phantom.jl")
+
+# Complex Structs
+abstract type AbstractWave end 
+include("structs/Apodization.jl")
+include("structs/Wave.jl")
 
 
 
