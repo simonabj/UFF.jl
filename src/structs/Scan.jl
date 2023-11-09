561ae75ae5749b713a3f4c2bd7cb8eae7305330a
Simon Andreas Bjørn <simonabj@ifi.uio.no>
2023-11-09 13:17:22 +0100

    Implement dependent properties for Scan

--- src/structs/Scan.jl
+++ src/structs/Scan.jl
@@ -2,21 +2,34 @@ export Scan, AbstractScan
 
 abstract type AbstractScan end
 
-#=
-    properties  (Access = public)
-        x                  % Vector containing the x coordinate of each pixel in the matrix
-        y                  % Vector containing the x coordinate of each pixel in the matrix
-        z                  % Vector containing the z coordinate of each pixel in the matrix
-    end
-    
-    properties  (Dependent)
-        N_pixels           % total number of pixels in the matrix
-        xyz                % location of the source [m m m] if the source is not at infinity    
-    end
-=#
-
 @kwdef mutable struct Scan <: AbstractScan
     x::Array{Float32, 1} = Array{Float32, 1}(undef, 0)
     y::Array{Float32, 1} = Array{Float32, 1}(undef, 0)
     z::Array{Float32, 1} = Array{Float32, 1}(undef, 0)
+end
+
+Base.propertynames(::Type{Scan}, private = false) = union(fieldnames(Scan), [:N_pixels, :xyz])
+
+function Base.getproperty(scan::Scan, s::Symbol) 
+    if s in fieldnames(Scan)
+        getfield(scan, s)
+    elseif s == :N_pixels
+        length(scan.x)
+    elseif s == :xyz
+        [scan.x[1], scan.y[1], scan.z[1]]
+    else
+        error("No get property $s exists in Scan")
+    end
+end
+
+function Base.setproperty!(scan::Scan, s::Symbol, v)
+    if s in fieldnames(Scan)
+        setfield!(scan, s, convert(fieldtype(Scan, s), v))
+    elseif s == :xyz
+        scan.x = v[:, 1]
+        scan.y = v[:, 2]
+        scan.z = v[:, 3]
+    else
+        error("No set property $s exists in Scan")
+    end
 end
