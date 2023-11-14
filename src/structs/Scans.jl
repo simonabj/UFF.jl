e05d0e0e2a7f6954ad48c92f6ebe6af9a4df2595
Simon Andreas Bjørn <simonabj@ifi.uio.no>
2023-11-15 00:53:56 +0100

    Update dependencies and add new scan types

--- src/structs/Scans.jl
+++ src/structs/Scans.jl
@@ -1,3 +1,5 @@
+using ArgCheck
+
 export CompositeScan, Scan, LinearScan, SectorScan, LinearScanRotated, Linear3DScan
 
 
@@ -11,6 +13,10 @@ end
 abstract type CompositeScan end
 Base.convert(::Type{Scan}, scan::CompositeScan) = scan.scan
 
+## Define composite scan types
+
+"""
+"""
 @kwdef mutable struct LinearScan <: CompositeScan
     scan::Scan = Scan()
 
@@ -18,10 +24,12 @@ Base.convert(::Type{Scan}, scan::CompositeScan) = scan.scan
     z_axis::Array{Float32, 1} = Array{Float32, 1}(undef, 0)
 end
 
+"""
+"""
 @kwdef mutable struct SectorScan <: CompositeScan
     scan::Scan = Scan()
 
-    origin::Vector{Point} = []
+    origin::Vector{Point} = [Point()]
     depth_axis::Vector{Float32} = []
     azimuth_axis::Vector{Float32} = []
 
@@ -30,6 +38,8 @@ end
     theta::Array{Float32, 1} = Array{Float32, 1}(undef, 0)
 end
 
+"""
+"""
 @kwdef mutable struct LinearScanRotated <: CompositeScan
     scan::Scan = Scan()
 
@@ -39,6 +49,8 @@ end
     center_of_rotation::Point = Point()
 end
 
+"""
+"""
 @kwdef mutable struct Linear3DScan <: CompositeScan
     scan::Scan = Scan() 
 
@@ -55,11 +67,13 @@ Base.propertynames(::LinearScan, private::Bool = false) = union(
     fieldnames(LinearScan), propertynames(Scan()) , [:N_x_axis, :N_z_axis, :x_step, :z_step, :reference_distance]
 )
 Base.propertynames(::SectorScan, private::Bool = false) = union(
-    fieldnames(SectorScan), [:N_azimuth_axis, :N_depth_axis, :N_origins, :depth_step, :reference_distance]
+    fieldnames(SectorScan), propertynames(Scan()), [:N_azimuth_axis, :N_depth_axis, :N_origins, :depth_step, :reference_distance]
 )
+# TODO: Add property names for LinearScanRotated
 Base.propertynames(::LinearScanRotated, private::Bool = false) = union(
-    fieldnames(LinearScanRotated), [:Not_finished]
+    fieldnames(LinearScanRotated), propertynames(Scan()), [:N_x_axis, :N_z_axis, :x_step, :z_step, :reference_distance]
 )
+# TODO: Add property names for Linear3DScan
 Base.propertynames(::Linear3DScan, private::Bool = false) = union(
     fieldnames(Linear3DScan), [:Not_finished]
 )
@@ -86,6 +100,8 @@ function Base.getproperty(scan::CompositeScan, s::Symbol)
         get_LinearScan_dependents(scan, s)
     elseif scan isa SectorScan
         get_SectorScan_dependents(scan, s)
+    elseif scan isa LinearScanRotated
+        get_LinearScanRotated_dependents(scan, s)
     elseif s == :Not_finished
         false
     else
@@ -118,6 +134,8 @@ function Base.setproperty!(scan::CompositeScan, s::Symbol, value)
         update_LinearScan_pixels!(scan) 
     elseif scan isa SectorScan
         update_SectorScan_pixels!(scan)
+    elseif scan isa LinearScanRotated
+        update_LinearScanRotated_pixels!(scan)
     end
 end
 
@@ -155,6 +173,22 @@ function get_SectorScan_dependents(sca::SectorScan, s::Symbol)
     end
 end
 
+function get_LinearScanRotated_dependents(scan::LinearScanRotated, s::Symbol)
+    if s == :N_x_axis
+        length(scan.x_axis)
+    elseif s == :N_z_axis
+        length(scan.z_axis)
+    elseif s == :x_step
+        sca.x_axis[2] - sca.x_axis[1]
+    elseif s == :z_step
+        sca.z_axis[2] - sca.z_axis[1]
+    elseif s == :reference_distance
+        sin(scan.rotation_angle)*scan.x + cos(scan.rotation_angle)*scan.z
+    else
+        error("No get property $s exists in LinearScanRotated")
+    end
+end
+
 ## Concrete setters
 
 ## Concrete updaters
@@ -165,7 +199,42 @@ function update_LinearScan_pixels!(scan::LinearScan)
 end
 
 function update_SectorScan_pixels!(scan::SectorScan)
-    assert(scan.N_origins == 1 || scan.N_origins == scan.N_azimuth_axis,
+    @argcheck scan.N_origins == 1 || scan.N_origins == scan.N_azimuth_axis ArgumentError(
         "Number of origins should be either one or equal to the number of scan lines")
+
+    N_pixels = scan.N_azimuth_axis * scan.N_depth_axis
+    x0 = getproperty.(scan.origin, :x)
+    y0 = getproperty.(scan.origin, :y)
+    z0 = getproperty.(scan.origin, :z)
+
+    scan.scan.x = (scan.depth_axis' .* sin.(scan.azimuth_axis)  .+ x0)[:]
+    scan.scan.z = (scan.depth_axis  .* cos.(scan.azimuth_axis)' .+ z0)[:]
+    scan.scan.y = zeros(N_pixels)
+end
+
+function update_LinearScanRotated_pixels!(scan::LinearScanRotated)
+    # Mesh grid
+    X = (ones(length(scan.z_axis))' .* scan.x_axis )[:];
+    Z = (ones(length(scan.x_axis))  .* scan.z_axis')[:];
+
+    angle = scan.rotation_angle
+    center = scan.center_of_rotation.xyz
+
+    if !isempty(X)
+        Xc = X .- center[1];
+        Zc = Z .- center[3];
+
+        rot_matrix = [cos(angle) -sin(angle); sin(angle) cos(angle)]
+        rot_grid = [Xc[:] Zc[:]]*rot_matrix
+        
+        X = reshape(rot_grid[:, 1], size(Xc))
+        Z = reshape(rot_grid[:, 2], size(Zc))
+
+        X = X .+ center[1]
+        Z = Z .+ center[3]
     
+        scan.scan.x = X[:]
+        scan.scan.y = zeros(prod(size(X)))
+        scan.scan.z = Z[:]
+    end
 end
