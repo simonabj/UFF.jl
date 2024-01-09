9710c1c4230cdaa70b15752ed1581d8e29ae55c1
Simon Andreas Bjørn <simonabj@ifi.uio.no>
2024-01-09 16:30:14 +0100

    Start implementing compute of Apo-Data

--- src/structs/ApertureApodization.jl
+++ src/structs/ApertureApodization.jl
@@ -7,20 +7,19 @@ end
 @kwdef mutable struct ApertureApodization
     probe::Probe = Probe()
     focus::Scan = Scan()
+    origin::Point = Point()
 
     window::Window.WindowType = Window.None
-    f_number::Vector{Float32} = [1.0, 1.0]
+    f_number::Tuple{Float32} = (1.0, 1.0)
 
     MLA::Integer = 1
     MLA_overlap::Integer = 0
-    
-    tilt::Vector{Float32} = [0.0, 0.0]
-    minimum_aperture::Vector{Float32} = [1e-3, 1e-3]
-    maximum_aperture::Vector{Float32} = [10, 10]
+
+    tilt::Tuple{Float32} = (0.0, 0.0)
+    minimum_aperture::Tuple{Float32} = (1e-3, 1e-3)
+    maximum_aperture::Vector{Float32} = (10, 10)
 
     apodization_vector::Vector{Float32} = []
-    origin::Point = Point()
-
 end
 
 
@@ -51,3 +50,29 @@ end
 
 # Calculate apodization data
 
+function compute(apod::ApertureApodization)
+    if isempty(apod.focus)
+        apod.focus = Scan([0], [0], [0])
+    end
+
+    if !isempty(apod.apodization_vector)
+        if length(apod.apodization_vector) == apod.probe.N_elements
+            return ApodizationData(ones(Float32, apod.focus.N_pixels, 1) * apod.apodization_vector')
+        else
+            return ApodizationData(reshape(apod.apodization_vector, :, 1))
+        end
+    end
+
+    if apod.window == Window.None
+        return ApodizationData(ones(Float32, apod.focus.N_pixels, apod.probe.N_elements))
+    
+    # STA apodization (Use the closest to user set origin)
+    elseif apod.window == Window.Sta
+        dist = mapslices(norm,apod.probe.xyz .- apod.origin.xyz', dims=2)
+        return ApodizationData(ones(Float32, apod.focus.N_pixels, 1) * (dist ≈ minimum(dist)))
+    else
+        # TODO: finish implementation. line 308 in +uff/apodization.m
+    end
+
+    return ApodizationData(ones(Float32, apod.focus.N_pixels, apod.probe.N_elements));
+end
